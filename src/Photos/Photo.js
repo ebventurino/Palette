8b1b877352e5b2ff022b1e45e5af00632d610c68
Emily Venturino <ebventurino@gmail.com>
2018-07-31 09:19:00 -0500

    favorites are saving to favorite list

--- src/Photos/Photo.js
+++ src/Photos/Photo.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-const Photo = props => (
+//
+const Photo = props => {
+  console.log(props)
+ const gettingPhotos = {
+   photoId: props.photo.id,
+   photoUrl: props.url
+ }
+ console.log(gettingPhotos,"gettingPhotos")
+
+  return (
   <li className="gif-wrap">
     <img src={props.url}alt=""/>
+    <label>
+    Favorite:
+<input onClick={() => props.loadingFavorites(gettingPhotos)}
+          name="favorite"
+          type="checkbox"
+          // checked={props.task.completeTask}
+          />
+   </label>
     </li>
-);
 
+);
+}
 export default Photo;
--- src/Photos/Photos.js
+++ src/Photos/Photos.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import '../App.css';
 import axios from 'axios'
-import PhotoList from './PhotoList';
+import SinglePhoto from './SinglePhoto';
 import logo from '../PaletteLogo.jpg'
-import Photo from './Photo'
 
 export default class Photos extends Component {
 
@@ -11,11 +10,12 @@ export default class Photos extends Component {
     super();
     this.state = {
       photos: [],
-      counter: 2
+      counter: 1
 
     };
   }
-//getting main API
+  
+// main api
   componentDidMount() {
     axios.get('https://api.unsplash.com/photos/?client_id=34b1a6b2a6c2a9b3f6af5e39a76c9943bdbbaf10bd455336f70e98ca31250475&per_page=9')
       .then(response => {
@@ -28,17 +28,16 @@ export default class Photos extends Component {
       });
   }
 
-//getting more Photos API
+// More Photos API
   getMorePhotos = () => {
     console.log(this.state.counter)
     this.setState({ counter: this.state.counter+1})
-    fetch(`https://api.unsplash.com/photos/?client_id=34b1a6b2a6c2a9b3f6af5e39a76c9943bdbbaf10bd455336f70e98ca31250475&page=${this.state.counter}`)
+    fetch(`https://api.unsplash.com/photos/?client_id=34b1a6b2a6c2a9b3f6af5e39a76c9943bdbbaf10bd455336f70e98ca31250475&per_page=9&page=${this.state.counter}`)
       .then(e => e.json())
       .then(photos => this.setState({ photos: photos }))
     }
 
-
-
+// header and posting state?
   render() {
     console.log(this.state.photos)
     return (
@@ -51,7 +50,7 @@ export default class Photos extends Component {
           <button onClick={this.getMorePhotos}>More Images</button>
         </div>
         <div className="main-content">
-          <PhotoList data={this.state.photos} />
+          <SinglePhoto data={this.state.photos} />
 
         </div>
       </div>
