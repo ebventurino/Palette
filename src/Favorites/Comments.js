3db27a218738b52a688b497aa3356985fe751fd1
Emily Venturino <ebventurino@gmail.com>
2018-08-06 14:03:53 -0500

    delete is working

--- src/Favorites/Comments.js
+++ src/Favorites/Comments.js
@@ -9,9 +9,9 @@ import axios from 'axios'
 export default class Comments extends Component {
     state = {
         comments: [],
-        commentsToEdit: {...this.props.comments},
+        commentsToEdit: { ...this.props.comments },
         viewForm: false,
-    
+
     }
     // main API
     componentDidMount() {
@@ -32,7 +32,7 @@ export default class Comments extends Component {
             // Once the new array of meals is retrieved, set the state
             .then(a => a.json())
             .then(comments => {
-                this.setState({CommentsToEdit: comments, viewForm: true})
+                this.setState({ CommentsToEdit: comments, viewForm: true })
             })
     }
 
@@ -46,8 +46,8 @@ export default class Comments extends Component {
             .then(Response => {
                 ApiManager.getAllComments()
             })
-           
-            
+
+
 
     }
 
@@ -62,43 +62,54 @@ export default class Comments extends Component {
         const stateToChange = {}
         stateToChange[e.target.id] = e.target.value
         this.setState(stateToChange)
-        
+
         ApiManager.getAllComments()
-        .then(comments => this.setState({ comments: comments }))
+            .then(comments => this.setState({ comments: comments }))
 
     }
     deleteComment = (commentId) => {
-        ApiManager.deleteComment(commentId) 
-            .then(deletedComment => this.setState({ comments: deletedComment }))
-    }
+        ApiManager.deleteComment(commentId)
+            .then(() => {
+                return ApiManager.getAllComments()
+            })
+            .then(Comments => {
+                console.log(Comments, 'comments')
+                this.setState({
+                    comments: Comments
+                });
+            });
 
+    }
 
 
 
     render() {
+
         return (
 
             <div className="comments">
-           
+
                 <form onSubmit={this.addComment.bind(this)}>
                     <label>
                         <textarea onChange={this.commentFormInput} type="textarea" rows="5" cols="80" name="Comment" id="message"
                             placeholder="Notes"
-                            required="" autoFocus=""  />
+                            required="" autoFocus="" />
                     </label>
                     <button type="submit" value="Submit" className="submit" onClick={this.commentFormInput}>Add Comment</button>
-                    
-                    </form>
-                   
+
+                </form>
+
 
                 {
+
                     this.state.comments.map(message =>
                         <CommentList
-                         key={message.id}
-                         message={message} 
-                         deleteComment={this.deleteComment}/>
+                            key={message.id}
+                            message={message}
+                            deleteComment={this.deleteComment} />
+
                     )
-                    
+
                 }
             </div>
         )
